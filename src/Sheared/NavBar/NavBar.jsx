e759d17645de72263f68cdbe269923dd62595a31
Afnan Sayed <afnansayed1973@gimal.com>
2024-08-15 00:23:07 -0700

    Fixing nav related issues

--- src/Sheared/NavBar/NavBar.jsx
+++ src/Sheared/NavBar/NavBar.jsx
@@ -1,28 +1,59 @@
 import { useContext } from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../../Providers/AuthProviders/AuthProviders";
 
 const NavBar = () => {
-
-  const {logout} = useContext(AuthContext);
+  const { logout,user } = useContext(AuthContext);
 
   const handleLogout = () => {
     logout();
-  }
+  };
 
-    const navItems = <>
-        <NavLink to='/'  className={({isActive}) => (isActive ? 'border-b-2 border-purple-600 font-bold px-3' : 'font-bold px-3')}> <li>Home</li></NavLink>
+  const navItems = (
+    <>
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive
+            ? "border-b-2 border-purple-600 font-bold px-3"
+            : "font-bold px-3"
+        }
+      >
+        {" "}
+        <li>Home</li>
+      </NavLink>
 
-        <NavLink to='/login'  className={({isActive}) => (isActive ? 'border-b-2 border-purple-600 font-bold px-3' : 'font-bold px-3')}> <li>Login</li></NavLink>
-        
-        <NavLink to='/register'  className={({isActive}) => (isActive ? 'border-b-2 border-purple-600 font-bold px-3' : 'font-bold px-3')}> <li>Sign Up</li></NavLink>
-        </>
+      <NavLink
+        to="/login"
+        className={({ isActive }) =>
+          isActive
+            ? "border-b-2 border-purple-600 font-bold px-3"
+            : "font-bold px-3"
+        }
+      >
+        {" "}
+        <li>Login</li>
+      </NavLink>
+
+      <NavLink
+        to="/register"
+        className={({ isActive }) =>
+          isActive
+            ? "border-b-2 border-purple-600 font-bold px-3"
+            : "font-bold px-3"
+        }
+      >
+        {" "}
+        <li>Sign Up</li>
+      </NavLink>
+    </>
+  );
   return (
     <>
       <div className="navbar bg-base-100">
         <div className="navbar-start">
           <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <div tabIndex={0} role="button" className="mr-5 mt-2 lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -45,15 +76,25 @@ const NavBar = () => {
               {navItems}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+            <p className="text-2xl font-semibold bg-gradient-to-r from-purple-600 via-red-400 to-purple-300 bg-clip-text text-transparent">Fashion<span className="text-3xl font-bold">X</span></p>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal ">
-              {navItems}
-          </ul>
+          <ul className="menu menu-horizontal ">{navItems}</ul>
         </div>
         <div className="navbar-end">
-          <button onClick={handleLogout} type="button" className="px-8 py-3  border-2 rounded border-purple-600 text-gray-800 font-bold">Border</button>
+          {user ? <button
+              onClick={handleLogout}
+              type="button"
+              className="px-8 py-3  border-2 rounded border-purple-600 text-gray-800 font-bold"
+            >
+              Logout
+            </button>:<button
+              onClick={handleLogout}
+              type="button"
+              className="px-8 py-3  border-2 rounded border-purple-600 text-gray-800 font-bold"
+            >
+            <Link to='/login'>Login</Link>
+            </button>}
         </div>
       </div>
     </>
